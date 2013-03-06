dbf0128b81de30c3791add80dc27098ae84f7c75
Dao Hoang Son <daohoangson@gmail.com>
2013-03-06 19:17:08 +0700

    [xenforo] properly prepare redirect uri for account/authorize view

--- xenforo/library/bdApi/ViewPublic/Account/Authorize.php
+++ xenforo/library/bdApi/ViewPublic/Account/Authorize.php
@@ -11,5 +11,11 @@ class bdApi_ViewPublic_Account_Authorize extends XenForo_ViewPublic_Base
 			$this->_params['authorizeScopes'] = explode(',', $this->_params['authorizeParams']['scope']);
 			$this->_params['authorizeScopes'] = array_map('trim', $this->_params['authorizeScopes']);
 		}
+		
+		$this->_params['authorizeRedirectUri'] = '';
+		if (!empty($this->_params['authorizeParams']['redirect_uri']))
+		{
+			$this->_params['authorizeRedirectUri'] = $this->_params['authorizeParams']['redirect_uri'];
+		}
 	}
 }
