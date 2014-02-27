1b2a3b811949d1b697d3f62cdb9a77c936f0bb9d
Dao Hoang Son <daohoangson@gmail.com>
2014-02-27 16:13:51 +0700

    [wordpress_consumer] v1.0.1 -- version bump

--- wordpress_consumer/xenforo-api-consumer/xenforo-api-consumer.php
+++ wordpress_consumer/xenforo-api-consumer/xenforo-api-consumer.php
@@ -3,7 +3,7 @@
  Plugin Name: XenForo API Consumer
  Plugin URI: https://xfrocks.com/api-support/
  Description: Connects to XenForo API system.
- Version: 1.0.0
+ Version: 1.0.1
  Author: XFROCKS
  Author URI: https://xfrocks.com
  Text Domain: xenforo-api-consumer
