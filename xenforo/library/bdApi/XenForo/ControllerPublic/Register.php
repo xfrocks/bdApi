2709d78c8910ef33fd18905a5c66b54c26cdc0d8
Dao Hoang Son <daohoangson@gmail.com>
2014-03-27 13:45:40 +0700

    [xenforo] implemented workaround for http://xenforo.com/community/threads/71307/

--- xenforo/library/bdApi/XenForo/ControllerPublic/Register.php
+++ xenforo/library/bdApi/XenForo/ControllerPublic/Register.php
@@ -1,7 +1,31 @@
 <?php
+
 class bdApi_XenForo_ControllerPublic_Register_Base extends XFCP_bdApi_Xenforo_ControllerPublic_Register
 {
 	private $_authorizePending = false;
+
+	public function actionFacebook()
+	{
+		// trying to workaround this bug http://xenforo.com/community/threads/71307/
+		$redirect = XenForo_Application::getSession()->get('fbRedirect');
+		if (empty($redirect))
+		{
+			// since 1.3.0 Gold
+			$redirect = XenForo_Application::getSession()->get('loginRedirect');
+		}
+
+		$response = parent::actionFacebook();
+
+		if (XenForo_Application::$versionId <= 1030070 AND $response instanceof XenForo_ControllerResponse_Redirect)
+		{
+			if ($response->redirectType == XenForo_ControllerResponse_Redirect::SUCCESS AND !empty($redirect))
+			{
+				$response->redirectTarget = $redirect;
+			}
+		}
+
+		return $response;
+	}
 
 	protected function _preDispatch($action)
 	{
@@ -13,8 +37,7 @@ class bdApi_XenForo_ControllerPublic_Register_Base extends XFCP_bdApi_Xenforo_Co
 
 	protected function _bdApi_responseView($viewName, $templateName, array $params, array $containerParams)
 	{
-		if ($viewName == 'XenForo_ViewPublic_Register_Process'
-		AND $templateName == 'register_process')
+		if ($viewName == 'XenForo_ViewPublic_Register_Process' AND $templateName == 'register_process')
 		{
 			// this means registration succeeded
 			// built-in or Facebook (or even our own [bd] API Consumer)
@@ -23,15 +46,13 @@ class bdApi_XenForo_ControllerPublic_Register_Base extends XFCP_bdApi_Xenforo_Co
 				// but we have an authorize request pending...
 				// redirect to that instead
 				// TODO: drop the usage of Xenforo_Controller::responseRedirect? A bit dangerous
-				return $this->responseRedirect(
-					XenForo_ControllerResponse_Redirect::RESOURCE_CREATED,
-					$this->_authorizePending
-				);
+				return $this->responseRedirect(XenForo_ControllerResponse_Redirect::RESOURCE_CREATED, $this->_authorizePending);
 			}
 		}
 
 		return parent::responseView($viewName, $templateName, $params, $containerParams);
 	}
+
 }
 
 if (XenForo_Application::$versionId > 1020000)
@@ -42,7 +63,9 @@ if (XenForo_Application::$versionId > 1020000)
 		{
 			return $this->_bdApi_responseView($viewName, $templateName, $params, $containerParams);
 		}
+
 	}
+
 }
 else
 {
@@ -52,5 +75,7 @@ else
 		{
 			return $this->_bdApi_responseView($viewName, $templateName, $params, $containerParams);
 		}
+
 	}
+
 }
