41e80832db1e5e4fb975640baaced028cfee29c6
Dao Hoang Son <daohoangson@gmail.com>
2014-08-06 11:18:36 +0700

    [wordpress_consumer] fixed paragraph spacing with <p>

--- wordpress_consumer/xenforo-api-consumer/includes/sync/post.php
+++ wordpress_consumer/xenforo-api-consumer/includes/sync/post.php
@@ -321,9 +321,12 @@ function _xfac_syncPost_getPostBody($post)
 		$text = str_replace(']]>', ']]&gt;', $text);
 	}
 
+	// fix paragraph spacing from WordPress to XenForo
+	$text = str_replace('</p>', '</p><br />', $text);
+
 	if (!!get_option('xfac_sync_post_wp_xf_link'))
 	{
-		$text .= '<br /><br /><a href="' . get_permalink($post->ID) . '">' . __('Read the whole post here.', 'xenforo-api-consumer') . '</a>';
+		$text .= '<br /><a href="' . get_permalink($post->ID) . '">' . __('Read the whole post here.', 'xenforo-api-consumer') . '</a>';
 	}
 
 	return $text;
