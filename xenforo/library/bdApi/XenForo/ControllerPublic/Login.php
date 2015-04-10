ebdd45a41da29132017165e25e5161cf28ead1e4
Dao Hoang Son <daohoangson@gmail.com>
2015-04-11 05:30:16 +0700

    [xenforo] added more information regarding login/logout via api

--- xenforo/library/bdApi/XenForo/ControllerPublic/Login.php
+++ xenforo/library/bdApi/XenForo/ControllerPublic/Login.php
@@ -12,18 +12,18 @@ class bdApi_XenForo_ControllerPublic_Login extends XFCP_bdApi_XenForo_Controller
 
         $userId = intval(bdApi_Crypt::decryptTypeOne($input['user_id'], $input['timestamp']));
 
-        $userModel = $this->_getUserModel();
+        if ($userId > 0) {
+            $this->_response->setHeader('X-Api-Login-User', $userId);
 
-        $userModel->setUserRememberCookie($userId);
+            $this->_getUserModel()->setUserRememberCookie($userId);
 
-        XenForo_Model_Ip::log($userId, 'user', $userId, 'login_api');
+            XenForo_Model_Ip::log($userId, 'user', $userId, 'login_api');
 
-        $userModel->deleteSessionActivity(0, $this->_request->getClientIp(false));
-
-        $session = XenForo_Application::get('session');
-
-        $session->changeUserId($userId);
-        XenForo_Visitor::setup($userId);
+            $this->_getUserModel()->deleteSessionActivity(0, $this->_request->getClientIp(false));
+            $session = XenForo_Application::get('session');
+            $session->changeUserId($userId);
+            XenForo_Visitor::setup($userId);
+        }
 
         if (empty($input['redirect'])) {
             $input['redirect'] = $this->getDynamicRedirectIfNot(XenForo_Link::buildPublicLink('login'));
--- xenforo/library/bdApi/XenForo/ControllerPublic/Logout.php
+++ xenforo/library/bdApi/XenForo/ControllerPublic/Logout.php
@@ -2,6 +2,25 @@
 
 class bdApi_XenForo_ControllerPublic_Logout extends XFCP_bdApi_XenForo_ControllerPublic_Logout
 {
+    protected $_bdApi_redirect = null;
+
+    public function actionIndex()
+    {
+        $userId = XenForo_Visitor::getUserId();
+
+        $response = parent::actionIndex();
+
+        if ($this->_bdApi_redirect !== null
+            && $response instanceof XenForo_ControllerResponse_Redirect
+            && $response->redirectTarget === $this->_bdApi_redirect
+        ) {
+            $this->_response->setHeader('X-Api-Logout-User', $userId);
+        }
+
+        return $response;
+    }
+
+
     public function getDynamicRedirect($fallbackUrl = false, $useReferrer = true)
     {
         $input = $this->_input->filter(array(
@@ -10,7 +29,10 @@ class bdApi_XenForo_ControllerPublic_Logout extends XFCP_bdApi_XenForo_Controlle
             'md5' => XenForo_Input::STRING,
         ));
 
-        if (!empty($input['md5']) AND md5($input['redirect']) === bdApi_Crypt::decryptTypeOne($input['md5'], $input['timestamp'])) {
+        if (!empty($input['md5'])
+            && md5($input['redirect']) === bdApi_Crypt::decryptTypeOne($input['md5'], $input['timestamp'])
+        ) {
+            $this->_bdApi_redirect = $input['redirect'];
             return $input['redirect'];
         }
 
