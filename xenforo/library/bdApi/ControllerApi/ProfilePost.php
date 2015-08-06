xfrocks/bdApi

commit 7b28da5d10ddc72ae59bf1bc46fd6b5fb5f796ec

[xenforo] fixed bug delete profile post comment

xenforo/library/bdApi/ControllerApi/ProfilePost.php
@@ -349,7 +349,7 @@ class bdApi_ControllerApi_ProfilePost extends bdApi_ControllerApi_Abstract
             return $this->responseNoPermission();
         }
 
-        if (!$this->_getProfilePostModel()->canDeleteProfilePostComment($comment, $profilePost, $user, $errorPhraseKey)) {
+        if (!$this->_getProfilePostModel()->canDeleteProfilePostComment($comment, $profilePost, $user, 'soft', $errorPhraseKey)) {
             throw $this->getErrorOrNoPermissionResponseException($errorPhraseKey);
         }
 

commit 3083cd1f74fdb643ed01c3636c28c2524e98a2e3

[xenforo] fixed XenForo 1.4 compatibility bug introduced by 7b28da5

xenforo/library/bdApi/ControllerApi/ProfilePost.php
@@ -363,7 +363,17 @@ class bdApi_ControllerApi_ProfilePost extends bdApi_ControllerApi_Abstract
             return $this->responseNoPermission();
         }
 
-        if (!$this->_getProfilePostModel()->canDeleteProfilePostComment($comment, $profilePost, $user, 'soft', $errorPhraseKey)) {
+        $errorPhraseKey = '';
+        if (XenForo_Application::$versionId > 1050051) {
+            $canDelete = call_user_func_array(array($this->_getProfilePostModel(), 'canDeleteProfilePostComment'), array(
+                $comment, $profilePost, $user, 'soft', &$errorPhraseKey
+            ));
+        } else {
+            $canDelete = call_user_func_array(array($this->_getProfilePostModel(), 'canDeleteProfilePostComment'), array(
+                $comment, $profilePost, $user, &$errorPhraseKey
+            ));
+        }
+        if (!$canDelete) {
             throw $this->getErrorOrNoPermissionResponseException($errorPhraseKey);
         }
 