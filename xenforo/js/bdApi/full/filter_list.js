e06318e07467ff258fb21250f54b132171e35773
Dao Hoang Son <daohoangson@gmail.com>
2017-08-14 16:53:52 +0700

    [xenforo] Remove filter_list.js

--- xenforo/js/bdApi/full/filter_list.js
+++
@@ -1,6 +0,0 @@
-! function ($, window, document, _undefined) {
-    // disable in-page filter
-    XenForo.FilterListItem.prototype.filter = function (filterRegex) {
-        return 1;
-    };
-}(jQuery, this, document);
