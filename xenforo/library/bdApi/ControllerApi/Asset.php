121d99c11501843d487c49ed04e29da33b209c5c
Dao Hoang Son <daohoangson@gmail.com>
2017-08-16 11:16:45 +0700

    [xenforo] Move sdk.js

--- xenforo/library/bdApi/ControllerApi/Asset.php
+++ xenforo/library/bdApi/ControllerApi/Asset.php
@@ -7,15 +7,19 @@ class bdApi_ControllerApi_Asset extends bdApi_ControllerApi_Abstract
         $prefix = $this->_input->filterSingle('prefix', XenForo_Input::STRING);
         $prefix = preg_replace('/[^a-zA-Z0-9]/', '', $prefix);
 
-        $sdkPath = XenForo_Autoloader::getInstance()->getRootDir() . '/../js/bdApi/full/sdk.js';
+        $sdkDirPath = preg_replace('/library.+$/', '', dirname(__FILE__));
+        $sdkPath = $sdkDirPath . 'js/bdApi/templates/sdk.js';
         $sdk = file_get_contents($sdkPath);
         $sdk = str_replace('{prefix}', $prefix, $sdk);
         $sdk = str_replace('{data_uri}', XenForo_Link::buildPublicLink('canonical:misc/api-data'), $sdk);
         $sdk = str_replace('{request_uri}', bdApi_Data_Helper_Core::safeBuildApiLink('index'), $sdk);
 
-        header('Content-Type: application/x-javascript; charset=utf-8');
-        header('Cache-Control: public, max-age=31536000');
-        header(sprintf('Last-Modified: %s', gmstrftime("%a, %d %b %Y %T %Z", filemtime($sdkPath))));
+        $ttl = 31536000;
+        $expires = XenForo_Application::$time + $ttl;
+        header('Content-Type: application/x-javascript; charset=utf-8', true);
+        header('Cache-Control: public, max-age=' . $ttl, true);
+        header(sprintf('Last-Modified: %s', gmstrftime("%a, %d %b %Y %T %Z", filemtime($sdkPath))), true);
+        header(sprintf('Expires: %s', gmstrftime("%a, %d %b %Y %T %Z", $expires)), true);
         die($sdk);
     }
 
