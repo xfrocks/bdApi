c1b196cd5835e11cff2e2e3a2eecede6db03b39e
Luu Truong <truonglv@outlook.com>
2018-06-07 16:02:19 +0700

    Allow set password if identity verified by provider.

--- xenforo_consumer/library/bdApiConsumer/XenForo/ControllerPublic/Account.php
+++ xenforo_consumer/library/bdApiConsumer/XenForo/ControllerPublic/Account.php
@@ -39,22 +39,7 @@ class bdApiConsumer_XenForo_ControllerPublic_Account extends XFCP_bdApiConsumer_
                 foreach ($visitor['externalAuth'] as $providerCode => $externalAuthId) {
                     $provider = bdApiConsumer_Option::getProviderByCode($providerCode);
                     if ($provider) {
-                        $parsed = @parse_url($provider['root']);
-                        $baseUrl = null;
-
-                        if (!empty($parsed['host'])) {
-                            $baseUrl = sprintf(
-                                '%s://%s%s',
-                                isset($parsed['schema']) ? $parsed['schema'] : 'http',
-                                $parsed['host'],
-                                isset($parsed['port']) ? (':' . $parsed['port']) : ''
-                            );
-                        }
-
-                        if ($baseUrl) {
-                            $provider['baseUrl'] = $baseUrl;
-                            $providers[$providerCode] = $provider;
-                        }
+                        $providers[$providerCode] = $provider;
                     }
                 }
 
@@ -63,6 +48,52 @@ class bdApiConsumer_XenForo_ControllerPublic_Account extends XFCP_bdApiConsumer_
         }
 
         return $response;
+    }
+
+    public function actionNewPassword()
+    {
+        $session = XenForo_Application::getSession();
+        $verified = $session->isRegistered('bdApiConsumer_verified') ? $session->get('bdApiConsumer_verified'): null;
+        // 5 minutes
+        $verifiedTtl = 5 * 60;
+
+        if (!$verified || ($verified + $verifiedTtl) <= XenForo_Application::$time) {
+            return $this->responseNoPermission();
+        }
+
+        $userId = XenForo_Visitor::getUserId();
+
+        if ($this->isConfirmedPost()) {
+            $input = $this->_input->filter(array(
+                'password' => XenForo_Input::STRING,
+                'password_confirm' => XenForo_Input::STRING
+            ));
+
+            $writer = XenForo_DataWriter::create('XenForo_DataWriter_User');
+            $writer->setExistingData($userId);
+            $writer->setPassword($input['password'], $input['password_confirm'], null, true);
+            $writer->save();
+            
+            $session = XenForo_Application::getSession();
+            if ($session->get('password_date')) {
+                $session->set('password_date', $writer->get('password_date'));
+            }
+
+            $session->remove('bdApiConsumer_verified');
+            $session->save();
+
+            return $this->responseRedirect(
+                XenForo_ControllerResponse_Redirect::SUCCESS,
+                $this->_buildLink('account/security')
+            );
+        }
+
+        $view = $this->responseView(
+            'bdApiConsumer_ViewPublic_Account_SecurityUpdate',
+            'bdapi_consumer_account_security_update'
+        );
+
+        return $this->_getWrapper('account', 'security', $view);
     }
 
     public function actionExternalAccounts()
@@ -114,6 +145,66 @@ class bdApiConsumer_XenForo_ControllerPublic_Account extends XFCP_bdApiConsumer_
         return $this->responseRedirect(
             XenForo_ControllerResponse_Redirect::RESOURCE_CANONICAL_PERMANENT,
             XenForo_Link::buildPublicLink('account/external-accounts')
+        );
+    }
+
+    public function actionConsumerSecurity()
+    {
+        $code = $this->_input->filterSingle('code', XenForo_Input::STRING);
+        $provider = bdApiConsumer_Option::getProviderByCode($code);
+
+        if (!$provider) {
+            return $this->responseNoPermission();
+        }
+
+        $visitor = XenForo_Visitor::getInstance();
+        /** @var bdApiConsumer_XenForo_Model_UserExternal $userExternalModel */
+        $userExternalModel = $this->getModelFromCache('XenForo_Model_UserExternal');
+        $externalData = $userExternalModel->getExternalAuthAssociationForUser(
+            $userExternalModel->bdApiConsumer_getProviderCode($provider),
+            $visitor['user_id']
+        );
+
+        if (!$externalData) {
+            return $this->responseNoPermission();
+        }
+
+        $extraData = XenForo_Helper_Php::safeUnserialize($externalData['extra_data']);
+
+        if ($this->isConfirmedPost()) {
+            $password = $this->_input->filterSingle('password', XenForo_Input::STRING);
+
+            $token = bdApiConsumer_Helper_Api::getAccessTokenFromUsernamePassword($provider, $extraData['username'], $password);
+            if (!$token) {
+                return $this->responseError(new XenForo_Phrase('your_existing_password_is_not_correct'));
+            }
+
+            $providerUser = bdApiConsumer_Helper_Api::getVisitor($provider, $token['access_token']);
+
+            if (!$providerUser) {
+                return $this->responseError(new XenForo_Phrase('your_existing_password_is_not_correct'));
+            }
+
+            $session = XenForo_Application::getSession();
+            $session->set('bdApiConsumer_verified', XenForo_Application::$time);
+            $session->save();
+
+            return $this->responseRedirect(
+                XenForo_ControllerResponse_Redirect::SUCCESS,
+                $this->_buildLink('account/new-password'),
+                new XenForo_Phrase('bdapi_consumer_your_identity_was_verified')
+            );
+        }
+
+        $viewParams = [
+            'provider' => $provider,
+            'extraData' => $extraData
+        ];
+
+        return $this->responseView(
+            'bdApiConsumer_ViewPublic_Account_Security',
+            'bdapi_consumer_account_security_verify',
+            $viewParams
         );
     }
 }
