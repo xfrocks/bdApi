b6b6d316d41051ef77c9df5f7a46d347f0928a10
Luu Truong <truonglv@outlook.com>
2018-07-10 14:46:01 +0700

    Add poll transform

---
+++ XF/Transform/PollResponse.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Xfrocks\Api\XF\Transform;
+
+use Xfrocks\Api\Transform\AbstractHandler;
+
+class PollResponse extends AbstractHandler
+{
+    const KEY_ID = 'response_id';
+    const KEY_ANSWER = 'response_answer';
+
+    const DYNAMIC_KEY_IS_VOTED = 'response_is_voted';
+
+    /**
+     * @var \XF\Entity\Poll|null
+     */
+    protected $poll;
+
+    public function getMappings()
+    {
+        return [
+            'poll_response_id' => self::KEY_ID,
+            'response' => self::KEY_ANSWER,
+
+            self::DYNAMIC_KEY_IS_VOTED
+        ];
+    }
+
+    public function calculateDynamicValue($key)
+    {
+        /** @var \XF\Entity\PollResponse $response */
+        $response = $this->source;
+
+        if ($this->poll === null) {
+            $this->poll = $this->app->em()->find('XF:Poll', $response->poll_id);
+        }
+
+        switch ($key) {
+            case self::DYNAMIC_KEY_IS_VOTED:
+                return $this->poll->hasVoted($response->poll_response_id);
+        }
+
+        return null;
+    }
+}
