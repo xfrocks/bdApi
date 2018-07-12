7e79b0b4fe2d5c6f76b31a45e3bd740ba7b8ceb1
Luu Truong <truonglv@outlook.com>
2018-07-12 17:16:04 +0700

    Dump modules version

--- xenforo/library/bdApi/ControllerApi/Index.php
+++ xenforo/library/bdApi/ControllerApi/Index.php
@@ -76,7 +76,7 @@ class bdApi_ControllerApi_Index extends bdApi_ControllerApi_Abstract
     protected function _getModules()
     {
         $modules = array(
-            'forum' => 2018060601,
+            'forum' => 2018071201,
             'oauth2' => 2016030902,
             'subscription' => 2014092301,
         );
