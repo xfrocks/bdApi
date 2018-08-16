d1f4aeefe982ed5446bc9815cb7df9e12031bef8
Dao Hoang Son <daohoangson@gmail.com>
2018-08-16 16:01:51 +0700

    Implement POST tools/link

--- Controller/Tool.php
+++ Controller/Tool.php
@@ -2,10 +2,35 @@
 
 namespace Xfrocks\Api\Controller;
 
+use Xfrocks\Api\Data\Param;
 use Xfrocks\Api\Util\Crypt;
 
 class Tool extends AbstractController
 {
+    public function actionPostLink()
+    {
+        $paramType = new Param('str', 'Link type (admin, api, or public)');
+        $paramType->options['default'] = 'public';
+        $paramRoute = new Param('str', 'Link route');
+        $paramRoute->options['default'] = 'index';
+        $params = $this->params()
+            ->define('type', $paramType)
+            ->define('route', $paramRoute);
+
+        switch ($params['type']) {
+            case 'admin':
+            case 'public':
+                $link = $this->app->router($params['type'])->buildLink($params['route']);
+                break;
+            case 'api':
+            default:
+                $link = $this->router()->buildLink($params['route']);
+                break;
+        }
+
+        return $this->api(['link' => $link]);
+    }
+
     public function actionPostPasswordTest()
     {
         $params = $this->params()
