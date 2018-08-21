9affb310a2eba0a3b85da8fcb504203d33076f29
Luu Truong <truonglv@outlook.com>
2018-08-21 10:27:16 +0700

    Added api POST `/posts/` and POST `/posts/attachments`

--- Controller/Post.php
+++ Controller/Post.php
@@ -3,6 +3,7 @@
 namespace Xfrocks\Api\Controller;
 
 use XF\Mvc\ParameterBag;
+use XF\Service\Thread\Replier;
 use Xfrocks\Api\Data\Params;
 use Xfrocks\Api\Util\PageNav;
 
@@ -75,6 +76,86 @@ class Post extends AbstractController
         ];
 
         return $this->api($data);
+    }
+
+    public function actionPostIndex()
+    {
+        $params = $this
+            ->params()
+            ->define('thread_id', 'uint', 'id of the target thread')
+            ->define('quote_post_id', 'uint', 'id of the quote post')
+            ->define('post_body', 'content of the new post')
+            ->defineAttachmentHash();
+
+        if (!empty($params['quote_post_id'])) {
+            $post = $this->assertViewablePost($params['quote_post_id']);
+            if ($params['thread_id'] > 0 && $post->thread_id !== $params['thread_id']) {
+                return $this->noPermission();
+            }
+
+            $thread = $post->Thread;
+            $postBody = $post->getQuoteWrapper($post->message) . $params['post_body'];
+        } else {
+            $thread = $this->assertViewableThread($params['thread_id']);
+
+            $postBody = $params['post_body'];
+        }
+
+        if (!$thread->canReply($error)) {
+            return $this->noPermission($error);
+        }
+
+        /** @var Replier $replier */
+        $replier = $this->service('XF:Thread\Replier', $thread);
+
+        $replier->setMessage($postBody);
+
+        /** @var \Xfrocks\Api\ControllerPlugin\Attachment $attachmentPlugin */
+        $attachmentPlugin = $this->plugin('Xfrocks\Api:Attachment');
+        $tempHash = $attachmentPlugin->getAttachmentTempHash([
+            'thread_id' => $thread->thread_id
+        ]);
+
+        $replier->setAttachmentHash($tempHash);
+
+        $replier->checkForSpam();
+
+        if (!$replier->validate($errors)) {
+            return $this->error($errors);
+        }
+
+        $post = $replier->save();
+
+        $data = [
+            'post' => $this->transformEntityLazily($post)
+        ];
+
+        return $this->api($data);
+    }
+
+    public function actionPostAttachments()
+    {
+        $params = $this
+            ->params()
+            ->defineFile('file', 'binary data of the attachment')
+            ->define('thread_id', 'uint', 'id of the container thread of the target post')
+            ->define('post_id', 'uint', 'id of the target post')
+            ->defineAttachmentHash();
+
+        if (empty($params['post_id']) && empty($params['thread_id'])) {
+            return $this->error(\XF::phrase('bdapi_slash_posts_attachments_requires_ids'), 400);
+        }
+
+        $context = [
+            'thread_id' => $params['thread_id'],
+            'post_id' => $params['post_id']
+        ];
+
+        /** @var \Xfrocks\Api\ControllerPlugin\Attachment $attachmentPlugin */
+        $attachmentPlugin = $this->plugin('Xfrocks\Api:Attachment');
+        $tempHash = $attachmentPlugin->getAttachmentTempHash($context);
+
+        return $attachmentPlugin->doUpload($tempHash, 'post', $context);
     }
 
     /**
