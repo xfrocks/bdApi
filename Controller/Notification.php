484a5c13b2da427d66c10a241bfeee7802323e99
Truong Luu <truonglv.9x@gmail.com>
2018-08-31 13:38:42 +0700

    Xf2 api notifications (#72)
    * Added api GET /notifications
    
    * Added API GET `/notifications/content`
    
    * Added API POST `/notifications/read`
    
    * Coding convention
    
    * Improvement action get noti content
    
    * Improvement get content
    
    * Improvement build batch option

---
+++ Controller/Notification.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace Xfrocks\Api\Controller;
+
+use XF\Entity\ConversationMaster;
+use XF\Entity\UserAlert;
+use XF\Mvc\Entity\Entity;
+use XF\Mvc\Entity\Finder;
+use XF\Mvc\ParameterBag;
+use Xfrocks\Api\Data\BatchJob;
+use Xfrocks\Api\Transformer;
+use Xfrocks\Api\Util\PageNav;
+
+class Notification extends AbstractController
+{
+    public function preDispatch($action, ParameterBag $params)
+    {
+        parent::preDispatch($action, $params);
+
+        $this->assertRegistrationRequired();
+    }
+
+    public function actionGetIndex()
+    {
+        $params = $this
+            ->params()
+            ->definePageNav();
+
+        /** @var \XF\Repository\UserAlert $alertRepo */
+        $alertRepo = $this->repository('XF:UserAlert');
+
+        $finder = $alertRepo->findAlertsForUser(
+            \XF::visitor()->user_id,
+            \XF::$time - $this->options()->alertExpiryDays * 86400
+        );
+
+        $params->limitFinderByPage($finder);
+
+        $total = $finder->total();
+        $notifications = $total > 0 ? $this->transformFinderLazily($finder) : [];
+
+        $data = [
+            'notifications' => $notifications,
+            'notifications_total' => $total
+        ];
+
+        PageNav::addLinksToData($data, $params, $total, 'notifications');
+
+        return $this->api($data);
+    }
+
+    public function actionPostRead()
+    {
+        $visitor = \XF::visitor();
+        if ($visitor->alerts_unread > 0) {
+            /** @var \XF\Repository\UserAlert $alertRepo */
+            $alertRepo = $this->repository('XF:UserAlert');
+            $alertRepo->markUserAlertsRead(\XF::visitor());
+        }
+
+        return $this->message(\XF::phrase('changes_saved'));
+    }
+
+    public function actionGetContent(ParameterBag $params)
+    {
+        /** @var UserAlert $alert */
+        $alert = $this->assertRecordExists('XF:UserAlert', $params->alert_id);
+        if ($alert->alerted_user_id !== \XF::visitor()->user_id) {
+            return $this->noPermission();
+        }
+
+        $jobConfig = $this->getBatchJobConfig($alert);
+        $contentResponse = null;
+
+        if ($jobConfig) {
+            $jobConfig = array_replace([
+                'method' => 'GET',
+                'uri' => null,
+                'params' => []
+            ], $jobConfig);
+
+            $job = new BatchJob($this->app, $jobConfig['method'], $jobConfig['params'], $jobConfig['uri']);
+            $contentResponse = $job->execute();
+        }
+
+        $data = [
+            'notification_id' => $alert->alert_id,
+            'notification' => $this->transformEntityLazily($alert)
+        ];
+
+        if ($contentResponse) {
+            /** @var Transformer $transformer */
+            $transformer = $this->app()->container('api.transformer');
+
+            $data = array_merge($data, $transformer->transformBatchJobReply($contentResponse));
+        }
+
+        return $this->api($data);
+    }
+
+    protected function getBatchJobConfig(UserAlert $alert)
+    {
+        switch ($alert->content_type) {
+            case 'conversation':
+                switch ($alert->action) {
+                    case 'insert':
+                    case 'join':
+                    case 'reply':
+                        return [
+                            'uri' => 'conversation-messages',
+                            'params' => [
+                                'conversation_id' => $alert->content_id
+                            ]
+                        ];
+                }
+
+                return [
+                    'uri' => 'conversations',
+                    'params' => [
+                        'conversation_id' => $alert->content_id
+                    ]
+                ];
+            case 'thread':
+                return [
+                    'uri' => 'threads',
+                    'params' => [
+                        'thread_id' => $alert->content_id
+                    ]
+                ];
+            case 'post':
+                return [
+                    'uri' => 'posts',
+                    'params' => [
+                        'page_of_post_id' => $alert->content_id
+                    ]
+                ];
+            case 'user':
+                switch ($alert->action) {
+                    case 'following':
+                        return [
+                            'uri' => 'users/followers',
+                            'params' => [
+                                'user_id' => $alert->content_id
+                            ]
+                        ];
+                    case 'post_copy':
+                    case 'post_move':
+                    case 'thread_merge':
+                        // TODO: Support user alert action (post_copy, post_move, thread_merge)
+                        break;
+                    case 'thread_move':
+                        break;
+                }
+
+                return [
+                    'uri' => 'users',
+                    'params' => [
+                        'user_id' => $alert->content_id
+                    ]
+                ];
+            case 'profile_post':
+                return [
+                    'uri' => 'profile-posts/comments',
+                    'params' => [
+                        'profile_post_id' => $alert->content_id
+                    ]
+                ];
+            case 'profile_post_comment':
+                return [
+                    'uri' => 'profile-posts/comments',
+                    'params' => [
+                        'page_of_comment_id' => $alert->content_id
+                    ]
+                ];
+        }
+
+        return null;
+    }
+}
---
+++ XF/Transform/UserAlert.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Xfrocks\Api\XF\Transform;
+
+use Xfrocks\Api\Transform\AbstractHandler;
+
+class UserAlert extends AbstractHandler
+{
+    const KEY_ID = 'notification_id';
+    const KEY_CREATE_DATE = 'notification_create_date';
+    const KEY_CREATOR_USER_ID = 'creator_user_id';
+    const KEY_CREATOR_USERNAME = 'creator_username';
+    const KEY_CONTENT_TYPE = 'content_type';
+    const KEY_CONTENT_ID = 'content_id';
+    const KEY_CONTENT_ACTION = 'content_action';
+
+    const DYNAMIC_KEY_IS_UNREAD = 'notification_is_unread';
+    const DYNAMIC_KEY_NOTIFICATION_TYPE = 'notification_type';
+    const DYNAMIC_KEY_NOTIFICATION_HTML = 'notification_html';
+
+    public function getMappings($context)
+    {
+        return [
+            'alert_id' => self::KEY_ID,
+            'event_date' => self::KEY_CREATE_DATE,
+            'user_id' => self::KEY_CREATOR_USER_ID,
+            'username' => self::KEY_CREATOR_USERNAME,
+            'content_type' => self::KEY_CONTENT_TYPE,
+            'content_id' => self::KEY_CONTENT_ID,
+            'action' => self::KEY_CONTENT_ACTION,
+
+
+            self::DYNAMIC_KEY_IS_UNREAD,
+            self::DYNAMIC_KEY_NOTIFICATION_TYPE,
+            self::DYNAMIC_KEY_NOTIFICATION_HTML
+        ];
+    }
+
+    public function calculateDynamicValue($context, $key)
+    {
+        /** @var \XF\Entity\UserAlert $alert */
+        $alert = $context->getSource();
+
+        switch ($key) {
+            case self::DYNAMIC_KEY_IS_UNREAD:
+                return $alert->isUnviewed();
+            case self::DYNAMIC_KEY_NOTIFICATION_TYPE:
+                return sprintf(
+                    '%s_%d_%s',
+                    $alert->content_type,
+                    $alert->content_id,
+                    $alert->action
+                );
+            case self::DYNAMIC_KEY_NOTIFICATION_HTML:
+                return $alert->render();
+        }
+
+        return parent::calculateDynamicValue($context, $key);
+    }
+
+    public function collectLinks($context)
+    {
+        /** @var \XF\Entity\UserAlert $alert */
+        $alert = $context->getSource();
+
+        $links = [];
+
+        $links['content'] = $this->buildApiLink('notifications/content', $alert);
+        $links['read'] = $this->buildApiLink('notifications/read');
+
+        /** @var \XF\Entity\User|null $user */
+        $user = $alert->User;
+        if ($user) {
+            $links['creator_avatar'] = $user->getAvatarUrl('m');
+        }
+
+        return $links;
+    }
+}
