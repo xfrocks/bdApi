295ad9f1bdbd2e1a559fddb7efd6e0366c2302c6
Dao Hoang Son <daohoangson@gmail.com>
2018-09-25 14:29:37 +0700

    Fix bug attachment uploading does not return data

--- _files/tests/api/ThreadTest.php
+++ _files/tests/api/ThreadTest.php
@@ -98,4 +98,26 @@ class ThreadTest extends ApiTestCase
 
         $this->assertArrayHasKey('post', $json);
     }
+
+    public function testPostAttachments()
+    {
+        $accessToken = self::$accessToken;
+        $fileName = 'white.png';
+        $forum = $this->dataForum();
+
+        $json = $this->httpRequestJson(
+            'POST',
+            "threads/attachments?forum_id={$forum['node_id']}&oauth_token={$accessToken}",
+            [
+                'body' => [
+                    'file' => fopen(__DIR__ . "/files/{$fileName}", 'r')
+                ]
+            ]
+        );
+
+        $this->assertArrayHasKey('attachment', $json);
+
+        $attachment = $json['attachment'];
+        $this->assertArrayHasKey('attachment_id', $attachment);
+    }
 }
