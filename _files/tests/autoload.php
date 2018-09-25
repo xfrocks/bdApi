908deba1254266baff986a3b8b73f625d2cf3f01
Dao Hoang Son <daohoangson@gmail.com>
2018-09-25 14:53:34 +0700

    Split add-on and tests dependencies

--- _files/tests/autoload.php
+++ _files/tests/autoload.php
@@ -4,7 +4,7 @@
 require('/var/www/html/src/vendor/autoload.php');
 
 /** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require(dirname(dirname(__DIR__)) . '/vendor/autoload.php');
+$loader = require(__DIR__ . '/vendor/autoload.php');
 
 $loader->addPsr4('tests\api\\', __DIR__ . '/api');
 $loader->addPsr4('tests\bases\\', __DIR__ . '/bases');
