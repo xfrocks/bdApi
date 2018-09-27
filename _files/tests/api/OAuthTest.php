00b75d5d56dd0c407975bd2c3aff8c574cd8ae7b
Luu Truong <truonglv@outlook.com>
2018-09-27 17:36:45 +0700

    Test case

--- _files/tests/api/OAuthTest.php
+++ _files/tests/api/OAuthTest.php
@@ -2,6 +2,8 @@
 
 namespace tests\api;
 
+use Base32\Base32;
+use Otp\Otp;
 use tests\bases\ApiTestCase;
 
 class OAuthTest extends ApiTestCase
@@ -35,5 +37,41 @@ class OAuthTest extends ApiTestCase
 
         $this->assertArrayHasKey('access_token', $json);
         $this->assertArrayHasKey('refresh_token', $json);
+    }
+
+    public function testGrantTypePasswordWithTfa()
+    {
+        $client = $this->dataApiClient();
+        $user = $this->dataUser(4);
+
+        $oAuthParams = [
+            'grant_type' => 'password',
+            'client_id' => $client['client_id'],
+            'client_secret' => $client['client_secret'],
+            'username' => $user['username'],
+            'password' => $user['password']
+        ];
+
+        $this->httpRequestJson('POST', 'oauth/token', [
+            'body' => $oAuthParams
+        ], false);
+
+        $response = $this->httpLatestResponse();
+        $this->assertEquals('202', strval($response->getStatusCode()), '202');
+
+        $this->assertNotEmpty($response->getHeader('X-Api-Tfa-Providers'));
+
+        $oAuthParams['tfa_provider'] = 'totp';
+
+        $otp = new Otp();
+        $oAuthParams['code'] = $otp->totp(Base32::decode($user['tfa_secret']));
+
+        $json = $this->postOauthToken($oAuthParams);
+
+        $this->assertArrayHasKey('access_token', $json);
+        $this->assertArrayHasKey('expires_in', $json);
+        $this->assertArrayHasKey('scope', $json);
+
+        $this->assertEquals($user['user_id'], $json['user_id']);
     }
 }
