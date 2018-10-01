0c3b290655ca0e01c88b780f1170e11759b78c2d
Truong Luu <truonglv.9x@gmail.com>
2018-10-01 10:00:01 +0700

    Subscription link bug (#82)
    * Fixed overwrite links
    
    * Subscription tests
    
    * No overwrite link hub
    
    * Fixed cannot unsubscribe in some case
    
    * Improvement subscription test case

--- Repository/Subscription.php
+++ Repository/Subscription.php
@@ -280,9 +280,9 @@ class Subscription extends Repository
             'oauth_token' => ''
         ]);
 
-        $response->header('Link', sprintf('<%s>; rel=hub', $hubLink));
+        $response->header('Link', sprintf('<%s>; rel=hub', $hubLink), false);
         if (!empty($selfLink)) {
-            $response->header('Link', sprintf('<%s>; rel=self', $selfLink));
+            $response->header('Link', sprintf('<%s>; rel=self', $selfLink), false);
         }
 
         if (!empty($subscriptionOption)) {
