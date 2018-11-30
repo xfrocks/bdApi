d23c8bee5446f888551b928950849d776acebf2e
Luu Truong <truonglv@outlook.com>
2018-11-30 14:21:07 +0700

    Added api `/lost-password`

---
+++ Controller/LostPassword.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Xfrocks\Api\Controller;
+
+use XF\Service\User\PasswordReset;
+
+class LostPassword extends AbstractController
+{
+    public function actionPostIndex()
+    {
+        $username = $this->filter('username', 'str');
+        $email = $this->filter('email', 'str');
+        $usernameOrEmail = $username ?: $email;
+
+        if (empty($usernameOrEmail)) {
+            return $this->error(\XF::phrase('bdapi_slash_lost_password_requires_username_or_email'), 400);
+        }
+
+        $token = $this->session()->getToken();
+        if (empty($token)) {
+            return $this->noPermission();
+        }
+
+        /** @var \XF\Repository\User $userRepo */
+        $userRepo = $this->repository('XF:User');
+        /** @var \XF\Entity\User|null $user */
+        $user = $userRepo->getUserByNameOrEmail($usernameOrEmail);
+        if (!$user) {
+            return $this->error(\XF::phrase('requested_member_not_found'));
+        }
+
+        /** @var PasswordReset $passwordReset */
+        $passwordReset = $this->service('XF:User\PasswordReset', $user);
+        if (!$passwordReset->canTriggerConfirmation($error)) {
+            return $this->error($error);
+        }
+
+        $passwordReset->triggerConfirmation();
+        return $this->message(\XF::phrase('password_reset_request_has_been_emailed_to_you'));
+    }
+}
