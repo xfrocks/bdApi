a9397bc21ae5812ad324e5fb625b3880b91ecb6c
datbth <datbth@gmail.com>
2018-12-01 13:39:45 +0700

    add actionIndex and actionSingle for albums

--- XFMG/Controller/Album.php
+++ XFMG/Controller/Album.php
@@ -5,11 +5,78 @@ namespace Xfrocks\Api\XFMG\Controller;
 
 use XF\Mvc\ParameterBag;
 use Xfrocks\Api\Controller\AbstractController;
+use Xfrocks\Api\Util\PageNav;
 
 class Album extends AbstractController
 {
     public function actionGetIndex(ParameterBag $params)
     {
-        return $this->api(['hello' => 'world']);
+        if ($params->album_id) {
+            return $this->actionSingle($params->album_id);
+        }
+
+        $params = $this->params()
+            ->defineOrder([
+                'natural' => ['create_date', 'asc'],
+                'natural_reverse' => ['create_date', 'desc'],
+                'album_last_update_date' => ['last_update_date', 'asc'],
+                'album_last_update_date_reverse' => ['last_update_date', 'desc'],
+                'album_rating' => ['rating_avg', 'asc'],
+                'album_rating_reverse' => ['rating_avg', 'desc'],
+                'album_comment_count' => ['comment_count', 'asc'],
+                'album_comment_count_reverse' => ['comment_count', 'desc'],
+            ])
+            ->definePageNav();
+
+        /** @var \XFMG\Finder\Album $finder */
+        $finder = $this->finder('XFMG:Album');
+        $params->sortFinder($finder);
+        $params->limitFinderByPage($finder);
+
+        $total = $finder->total();
+        $albums = $total > 0 ? $this->transformFinderLazily($finder) : [];
+
+        $data = [
+            'albums' => $albums,
+            'albums_total' => $total
+        ];
+
+        PageNav::addLinksToData($data, $params, $total, 'albums');
+
+        return $this->api($data);
+    }
+
+    protected function actionSingle($albumId)
+    {
+        $album = $this->assertViewableAlbum($albumId);
+
+        $data = [
+            'album' => $this->transformEntityLazily($album)
+        ];
+
+        return $this->api($data);
+    }
+
+    /**
+     * @param int $albumId
+     * @param array $extraWith
+     * @return \XFMG\Entity\Album
+     * @throws \XF\Mvc\Reply\Exception
+     */
+    protected function assertViewableAlbum($albumId, array $extraWith = [])
+    {
+        /** @var \XFMG\Entity\Album $album */
+        $album = $this->assertRecordExists(
+            'XFMG:Album',
+            $albumId,
+            $extraWith,
+            'xfmg_requested_album_not_found'
+        );
+
+        if (!$album->canView($error)) {
+            throw $this->exception($this->noPermission($error));
+        }
+
+        return $album;
     }
 }
---
+++ XFMG/Transform/Album.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: datbth
+ * Date: 01/12/2018
+ * Time: 10:50
+ */
+
+namespace Xfrocks\Api\XFMG\Transform;
+
+use Xfrocks\Api\Transform\AbstractHandler;
+
+class Album extends AbstractHandler
+{
+    const KEY_ID = 'album_id';
+    const KEY_CATEGORY_ID = 'category_id';
+    const KEY_TITLE = 'title';
+    const KEY_DESCRIPTION = 'description';
+    const KEY_CREATE_DATE = 'create_date';
+    const KEY_LAST_UPDATE_DATE = 'last_update_date';
+    const KEY_USER_ID = 'user_id';
+    const KEY_USERNAME = 'username';
+    const KEY_LIKE_COUNT = 'like_count';
+    const KEY_MEDIA_COUNT = 'media_count';
+    const KEY_VIEW_COUNT = 'view_count';
+    const KEY_RATING_COUNT = 'rating_count';
+    const KEY_RATING = 'rating';
+    const KEY_COMMENT_COUNT = 'comment_count';
+
+    public function collectLinks($context)
+    {
+        /** @var \XFMG\Entity\Album $album */
+        $album = $context->getSource();
+
+        $links = [
+            self::LINK_PERMALINK => $this->buildPublicLink('media/albums', $album),
+        ];
+
+        return $links;
+    }
+
+    public function getMappings($context)
+    {
+        return [
+            'album_id' => self::KEY_ID,
+            'category_id' => self::KEY_CATEGORY_ID,
+            'title' => self::KEY_TITLE,
+            'description' => self::KEY_DESCRIPTION,
+            'user_id' => self::KEY_USER_ID,
+            'username' => self::KEY_USERNAME,
+            'media_count' => self::KEY_MEDIA_COUNT,
+            'likes' => self::KEY_LIKE_COUNT,
+            'comment_count' => self::KEY_COMMENT_COUNT,
+            'rating_count' => self::KEY_RATING_COUNT,
+            'rating_avg' => self::KEY_RATING,
+        ];
+    }
+}
