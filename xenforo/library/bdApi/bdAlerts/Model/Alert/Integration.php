96debc0968c5aa4d5697a061a77841e37493fc5c
Dao Hoang Son <daohoangson@gmail.com>
2019-06-05 11:59:02 +0700

    Improve `markAlertRead` performance

--- xenforo/library/bdApi/bdAlerts/Model/Alert/Integration.php
+++ xenforo/library/bdApi/bdAlerts/Model/Alert/Integration.php
@@ -4,30 +4,35 @@ class bdApi_bdAlerts_Model_Alert_Integration extends XFCP_bdApi_bdAlerts_Model_A
 {
     public function markAlertRead(array $alert, $time = null)
     {
-        $visitor = XenForo_Visitor::getInstance();
-        $alertsUnread = $visitor['alerts_unread'];
-
-        parent::markAlertRead($alert, $time);
-
-        if ($visitor['alerts_unread'] !== $alertsUnread &&
-            bdApi_Option::getSubscription(bdApi_Model_Subscription::TYPE_NOTIFICATION)
-        ) {
-            /** @var bdApi_XenForo_Model_Alert $alertModel */
-            $alertModel = $this->getModelFromCache('XenForo_Model_Alert');
-            $userOption = $alertModel->bdApi_getUserNotificationOption($visitor['user_id']);
-            if (!empty($userOption)) {
-                /* @var $subscriptionModel bdApi_Model_Subscription */
-                $subscriptionModel = $this->getModelFromCache('bdApi_Model_Subscription');
-                $subscriptionModel->ping(
-                    $userOption,
-                    'read',
-                    bdApi_Model_Subscription::TYPE_NOTIFICATION,
-                    bdApi_AlertHandler_Ping::fakeAlert(
-                        $visitor['user_id'],
-                        array('read_notification_id' => $alert['alert_id'])
-                    )
-                );
-            }
+        $marked = parent::markAlertRead($alert, $time);
+        if (!$marked) {
+            return false;
         }
+
+        if (!bdApi_Option::getSubscription(bdApi_Model_Subscription::TYPE_NOTIFICATION)) {
+            return $marked;
+        }
+
+        /** @var bdApi_XenForo_Model_Alert $alertModel */
+        $alertModel = $this->getModelFromCache('XenForo_Model_Alert');
+        $userId = $alert['alerted_user_id'];
+        $userOption = $alertModel->bdApi_getUserNotificationOption($userId);
+        if (empty($userOption)) {
+            return $marked;
+        }
+
+        /* @var $subscriptionModel bdApi_Model_Subscription */
+        $subscriptionModel = $this->getModelFromCache('bdApi_Model_Subscription');
+        $subscriptionModel->ping(
+            $userOption,
+            'read',
+            bdApi_Model_Subscription::TYPE_NOTIFICATION,
+            bdApi_AlertHandler_Ping::fakeAlert(
+                $userId,
+                array('read_notification_id' => $alert['alert_id'])
+            )
+        );
+
+        return $marked;
     }
 }
