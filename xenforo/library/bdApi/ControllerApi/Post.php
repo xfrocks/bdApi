d23527e62ea7088005c0fd87b34a41c2213b914e
Truong Luu <truonglv@outlook.com>
2020-12-08 10:35:15 +0700

    Split check required post body (#148)

--- xenforo/library/bdApi/ControllerApi/Post.php
+++ xenforo/library/bdApi/ControllerApi/Post.php
@@ -237,13 +237,7 @@ class bdApi_ControllerApi_Post extends bdApi_ControllerApi_Abstract
             'quote_post_id' => XenForo_Input::UINT,
         ));
 
-        /* @var $editorHelper XenForo_ControllerHelper_Editor */
-        $editorHelper = $this->getHelper('Editor');
-        $input['post_body'] = $editorHelper->getMessageText('post_body', $this->_input);
-        $input['post_body'] = XenForo_Helper_String::autoLinkBbCode($input['post_body']);
-        if (!strlen(trim($input['post_body']))) {
-            return $this->responseError(new XenForo_Phrase('bdapi_slash_post_requires_post_body'), 400);
-        }
+        $input['post_body'] = $this->_assertPostBodyValid();
 
         $ftpHelper = $this->_getForumThreadPostHelper();
         if (!empty($input['quote_post_id'])) {
@@ -788,6 +782,21 @@ class bdApi_ControllerApi_Post extends bdApi_ControllerApi_Abstract
         }
 
         $fetchOptions['page'] = floor($position / $fetchOptions['limit']) + 1;
+    }
+
+    protected function _assertPostBodyValid()
+    {
+        /* @var $editorHelper XenForo_ControllerHelper_Editor */
+        $editorHelper = $this->getHelper('Editor');
+        $postBody = $editorHelper->getMessageText('post_body', $this->_input);
+        $postBody = XenForo_Helper_String::autoLinkBbCode($postBody);
+        if (!strlen(trim($postBody))) {
+            throw $this->responseException(
+                $this->responseError(new XenForo_Phrase('bdapi_slash_post_requires_post_body'), 400)
+            );
+        }
+
+        return $postBody;
     }
 
     protected function _preparePosts(array $posts, array $thread = null, array $forum = null)
