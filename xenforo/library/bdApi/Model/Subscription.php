9409705b0550bf9d43d5cc43b2fab102a3608fb5
Luu Truong <truonglv@outlook.com>
2021-06-04 08:10:57 +0700

    Update comment

--- xenforo/library/bdApi/Model/Subscription.php
+++ xenforo/library/bdApi/Model/Subscription.php
@@ -217,7 +217,7 @@ class bdApi_Model_Subscription extends XenForo_Model
             $realAlerts = $alertModel->bdApi_getAlertsByIds($alertIds);
             foreach ($realAlerts as $alertId => $alert) {
                 if (!empty($alert['view_date']) || !$alertModel->bdApi_canPushNotification($alert)) {
-                    // skip alert already read
+                    // skip alert already read or disabled to push
                     continue;
                 }
                 $alerts[$alertId] = $alert;
