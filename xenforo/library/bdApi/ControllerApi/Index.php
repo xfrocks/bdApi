7fa4f4b9383382dc4707780f937e64856baecb23
Đào Hoàng Sơn <daohoangson@gmail.com>
2022-02-16 16:56:03 +0700

    Update module version forum@2022021601

--- xenforo/library/bdApi/ControllerApi/Index.php
+++ xenforo/library/bdApi/ControllerApi/Index.php
@@ -76,7 +76,7 @@ class bdApi_ControllerApi_Index extends bdApi_ControllerApi_Abstract
     protected function _getModules()
     {
         $modules = array(
-            'forum' => 2020061901,
+            'forum' => 2022021601,
             'oauth2' => 2016030902,
             'subscription' => 2019040901,
         );
